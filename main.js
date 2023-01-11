ffa36ecd574be4b54ec90e5ee943f9d340c273a3
Tyron Love <hello@tyronlove.com>
2023-01-11 08:34:34 +0200

    rewrote description.

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import blog, { ga, redirects } from "https://deno.land/x/blog@0.5.0/blog.tsx";
 
 blog({
   title: "Tyron's Blog",
-  description: "Restarting my blogging career one commit at a time",
+  description: "Hi! I'm Tyron. Proud Dad, lover of one exquisite human, cartoonist for pretend.",
   author: "Tyron Love",
   avatar: "https://pbs.twimg.com/profile_images/1495138432959537156/1Z9AKCI4_400x400.jpg",
   avatarClass: "full",
