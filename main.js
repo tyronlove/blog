bcd8d73318e881d9771ba6c9a8528130bf88636c
Tyron Love <hello@tyronlove.com>
2023-02-09 08:10:04 +0200

    More fiddling with www redirect.

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import blog, { redirects } from "https://deno.land/x/blog@0.5.0/blog.tsx";
+import blog, { ga, redirects } from "https://deno.land/x/blog@0.5.0/blog.tsx";
 
 blog({
   title: "Tyron's Blog",
