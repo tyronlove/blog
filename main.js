720bc34d75fd5955d0ec0ee8bd4800b788f6acff
Tyron Love <calm.wing4709@fastmail.com>
2023-02-13 09:44:25 +0200

    Added main back

---
+++ main.js
@@ -0,0 +1,21 @@
+import blog, {} from "https://deno.land/x/blog@0.5.0/blog.tsx";
+
+blog({
+  title: "Tyron's Blog",
+  description: "Proud Dad. Lover of one exquisite human. Cartoonist for pretend.",
+  author: "Tyron Love",
+  avatar: "/images/profile.png",
+  avatarClass: "full",
+  links: [
+    { title: "Twitter", url: "https://twitter.com/tyron_love" },
+    { title: "Instagram", url: "https://www.instagram.com/tyronlove/" },
+    { title: "Github", url: "https://github.com/tyronlove" },
+  ],
+  lang: "en",
+  favicon: "favicon.ico",
+  ogImage: {
+    url: "/images/profile.png",
+    twitterCard:  "/images/profile.png" 
+  },
+  style:".text-lg {text-align: center!important;} ol, ul, menu {list-style: square!important;} .text-bluegray-500 { font-weight: 400;!important;}"
+});
